1d5c74761ccf66fcab239cd1edf08c5e06f04231
Damian Mooyman <damian@silverstripe.com>
2014-07-29 13:02:01 +1200

    Fix travis tests

--- code/handling/SpellException.php
+++ code/handling/SpellException.php
@@ -10,7 +10,7 @@ class SpellException extends Exception {
 	 * @param int $code HTTP error code
 	 * @param Exception $previous
 	 */
-	public function __construct($message, $code, $previous) {
+	public function __construct($message, $code, $previous = null) {
 		parent::__construct($message, $code, $previous);
 	}
 }
